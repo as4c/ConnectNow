f3127abb97cd9501cf50762959e2649dffb36b7c
sagar kumar <sagarkumarsameer@gmail.com>
2023-05-17 09:50:55 +0530

    production update

--- Connectnow/wsgi.py
+++ Connectnow/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Connectnow.settings')
+from decouple import config
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
 
 application = get_wsgi_application()
